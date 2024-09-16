6d7a58c5e36001ea4309d596499f0ba1808e4138
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-16 08:38:42 -0300

    first commit

---
+++ atv13.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+n = int(input("programa"))
+if n < 0:
+   print("negativo")
+if n == 0:
+    print("neutro")
+if n > 0:
+    print("positivo")
